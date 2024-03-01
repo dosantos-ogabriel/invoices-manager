6d4c3ff6463f56652bd787ba6552da88c54c2380
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2024-03-01 16:26:08 -0300

    use prisma with postgresql

--- server/api/payments/create.post.ts
+++ server/api/payments/create.post.ts
@@ -1,8 +1,7 @@
-import * as Payments from "~/server/src/payment";
-import { getDatabase } from "~/server/src/database";
+import payment from "~/server/src/payment";
 
 export default defineEventHandler(async (event) => {
-	const db = await getDatabase();
 	const body = await readBody(event);
-	return Payments.add(db, body);
+
+	return payment.addPayment(body);
 });
